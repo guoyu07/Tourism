349e8b052b631aacd2e41c8e3804887fd26b040f
Farid Rn <faridv@gmail.com>
2015-08-24 21:52:59 +0430

    Making panoramas more dynamic

--- modules/mod_panorama/tmpl/default.php
+++ modules/mod_panorama/tmpl/default.php
@@ -8,11 +8,446 @@
  * @license 
  */
 defined('_JEXEC') or die('Restricted access');
+
+$data = '[
+    {
+        "en": "Abyaneh",
+        "fa": "",
+        "img": "Abyaneh.jpg"
+    },
+    {
+        "en": "Ali Qapoo",
+        "fa": "",
+        "img": "Ali Qapoo.jpg"
+    },
+    {
+        "en": "Amir Chakhmaq Square",
+        "fa": "",
+        "img": "Amir Chakhmaq Square.jpg"
+    },
+    {
+        "en": "Aqa Bozorg Mosque and School",
+        "fa": "",
+        "img": "Aqa Bozorg Mosque and School.jpg"
+    },
+    {
+        "en": "Ardestan Jame\' Mosque",
+        "fa": "",
+        "img": "Ardestan Jame\' Mosque.jpg"
+    },
+    {
+        "en": "Ardestan Jame\' Mosque (2)",
+        "fa": "",
+        "img": "Ardestan Jame\' Mosque (2).jpg"
+    },
+    {
+        "en": "Arg-e Bam (Bam Citadel)",
+        "fa": "",
+        "img": "Arg-e Bam (Bam Citadel).jpg"
+    },
+    {
+        "en": "Arg-e Karim khan (Karim Khan\'s Citadel)",
+        "fa": "",
+        "img": "Arg-e Karim khan (Karim Khan\'s Citadel).jpg"
+    },
+    {
+        "en": "Arg-e Rayen (Rayen Castle)",
+        "fa": "",
+        "img": "Arg-e Rayen (Rayen Castle).jpg"
+    },
+    {
+        "en": "Arg-e Rayen (Rayen Castle) (2)",
+        "fa": "",
+        "img": "Arg-e Rayen (Rayen Castle) (2).jpg"
+    },
+    {
+        "en": "Arjantin Square",
+        "fa": "",
+        "img": "Arjantin Square.jpg"
+    },
+    {
+        "en": "Baq-e Sangi (Stony Garden)",
+        "fa": "",
+        "img": "Baq-e Sangi (Stony Garden).jpg"
+    },
+    {
+        "en": "Bath of Fin Garden",
+        "fa": "",
+        "img": "Bath of Fin Garden.jpg"
+    },
+    {
+        "en": "Bath of the Amerees\' House",
+        "fa": "",
+        "img": "Bath of the Amerees\' House.jpg"
+    },
+    {
+        "en": "Bisotun Inscription",
+        "fa": "",
+        "img": "Bisotun Inscription.jpg"
+    },
+    {
+        "en": "Boroojerdis\' House",
+        "fa": "",
+        "img": "Boroojerdis\' House.jpg"
+    },
+    {
+        "en": "Carpet Shop",
+        "fa": "",
+        "img": "Carpet Shop.jpg"
+    },
+    {
+        "en": "Chehel Sotun (Forty Culumns)",
+        "fa": "",
+        "img": "Chehel Sotun (Forty Culumns).jpg"
+    },
+    {
+        "en": "Chehel Sotun (Forty Culumns) (2)",
+        "fa": "",
+        "img": "Chehel Sotun (Forty Culumns) (2).jpg"
+    },
+    {
+        "en": "Cinema Mellat",
+        "fa": "",
+        "img": "Cinema Mellat.jpg"
+    },
+    {
+        "en": "Dolat Abad Garden",
+        "fa": "",
+        "img": "Dolat Abad Garden.jpg"
+    },
+    {
+        "en": "Eram Garden",
+        "fa": "",
+        "img": "Eram Garden.jpg"
+    },
+    {
+        "en": "Fahraj Jame\' Mosque",
+        "fa": "",
+        "img": "Fahraj Jame\' Mosque.jpg"
+    },
+    {
+        "en": "Fin garden",
+        "fa": "",
+        "img": "Fin garden.jpg"
+    },
+    {
+        "en": "Firooz Abad Sasanid Castle",
+        "fa": "",
+        "img": "Firooz Abad Sasanid Castle.jpg"
+    },
+    {
+        "en": "Ganj Ali Khan Bathhouse",
+        "fa": "",
+        "img": "Ganj Ali Khan Bathhouse.jpg"
+    },
+    {
+        "en": "Ganj Ali Khan Square",
+        "fa": "",
+        "img": "Ganj Ali Khan Square.jpg"
+    },
+    {
+        "en": "Ganj-Ali Khan Mosque",
+        "fa": "",
+        "img": "Ganj-Ali Khan Mosque.jpg"
+    },
+    {
+        "en": "Gonbad Jabaliyeh",
+        "fa": "",
+        "img": "Gonbad Jabaliyeh.jpg"
+    },
+    {
+        "en": "Harandi Garden",
+        "fa": "",
+        "img": "Harandi Garden.jpg"
+    },
+    {
+        "en": "Hasht Behesht",
+        "fa": "",
+        "img": "Hasht Behesht.jpg"
+    },
+    {
+        "en": "Heydarzadeh\'s House",
+        "fa": "",
+        "img": "Heydarzadeh\'s House.jpg"
+    },
+    {
+        "en": "Imam Mosque",
+        "fa": "",
+        "img": "Imam Mosque.jpg"
+    },
+    {
+        "en": "Islamieh Village",
+        "fa": "",
+        "img": "Islamieh Village.jpg"
+    },
+    {
+        "en": "Islamieh Village (2)",
+        "fa": "",
+        "img": "Islamieh Village (2).jpg"
+    },
+    {
+        "en": "Jahan Nama Garden",
+        "fa": "",
+        "img": "Jahan Nama Garden.jpg"
+    },
+    {
+        "en": "Jamaran Hosseinieh",
+        "fa": "",
+        "img": "Jamaran Hosseinieh.jpg"
+    },
+    {
+        "en": "Ka\'be-ye Zartosht (Cube of Zoroaster)",
+        "fa": "",
+        "img": "Ka\'be-ye Zartosht (Cube of Zoroaster).jpg"
+    },
+    {
+        "en": "Kabood Mosque",
+        "fa": "",
+        "img": "Kabood Mosque.jpg"
+    },
+    {
+        "en": "Kerman Jame\' Mosque",
+        "fa": "",
+        "img": "Kerman Jame\' Mosque.jpg"
+    },
+    {
+        "en": "Kerman Jame\' Mosque (2)",
+        "fa": "",
+        "img": "Kerman Jame\' Mosque (2).jpg"
+    },
+    {
+        "en": "Khosro Mosque",
+        "fa": "",
+        "img": "Khosro Mosque.jpg"
+    },
+    {
+        "en": "Kooh Gol Lake",
+        "fa": "",
+        "img": "Kooh Gol Lake.jpg"
+    },
+    {
+        "en": "Larees\' House",
+        "fa": "",
+        "img": "Larees\' House.jpg"
+    },
+    {
+        "en": "Margoon Waterfall",
+        "fa": "",
+        "img": "Margoon Waterfall.jpg"
+    },
+    {
+        "en": "Milad Tower",
+        "fa": "",
+        "img": "Milad Tower.jpg"
+    },
+    {
+        "en": "Moavenol Molk Tekyeh",
+        "fa": "",
+        "img": "Moavenol Molk Tekyeh.jpg"
+    },
+    {
+        "en": "Money Museum",
+        "fa": "",
+        "img": "Money Museum.jpg"
+    },
+    {
+        "en": "Moshirol Mamalek Mosque",
+        "fa": "",
+        "img": "Moshirol Mamalek Mosque.jpg"
+    },
+    {
+        "en": "Na\'een Jame\' Mosque",
+        "fa": "",
+        "img": "Na\'een Jame\' Mosque.jpg"
+    },
+    {
+        "en": "Na\'een Jame\' Mosque (2)",
+        "fa": "",
+        "img": "Na\'een Jame\' Mosque (2).jpg"
+    },
+    {
+        "en": "Naqsh-e Rajab",
+        "fa": "",
+        "img": "Naqsh-e Rajab.jpg"
+    },
+    {
+        "en": "Narin Qal\'eh (Narin Castle)",
+        "fa": "",
+        "img": "Narin Qal\'eh (Narin Castle).jpg"
+    },
+    {
+        "en": "National Garden",
+        "fa": "",
+        "img": "National Garden.jpg"
+    },
+    {
+        "en": "Nobar Bathhouse",
+        "fa": "",
+        "img": "Nobar Bathhouse.jpg"
+    },
+    {
+        "en": "Old Alley",
+        "fa": "",
+        "img": "Old Alley.jpg"
+    },
+    {
+        "en": "Oljayto Sanctuary",
+        "fa": "",
+        "img": "Oljayto Sanctuary.jpg"
+    },
+    {
+        "en": "Pirnia house",
+        "fa": "",
+        "img": "Pirnia house.jpg"
+    },
+    {
+        "en": "Poets Mausoleum",
+        "fa": "",
+        "img": "Poets Mausoleum.jpg"
+    },
+    {
+        "en": "Qeisarieh Bazzar",
+        "fa": "",
+        "img": "Qeisarieh Bazzar.jpg"
+    },
+    {
+        "en": "Saint Stepanos Monastery",
+        "fa": "",
+        "img": "Saint Stepanos Monastery.jpg"
+    },
+    {
+        "en": "San\'at Square",
+        "fa": "",
+        "img": "San\'at Square.jpg"
+    },
+    {
+        "en": "Saraye Moshir",
+        "fa": "",
+        "img": "Saraye Moshir.jpg"
+    },
+    {
+        "en": "Seyyed Rokneddin Tomb",
+        "fa": "",
+        "img": "Seyyed Rokneddin Tomb.jpg"
+    },
+    {
+        "en": "Shazdeh Garden",
+        "fa": "",
+        "img": "Shazdeh Garden.jpg"
+    },
+    {
+        "en": "Sheikh Lotfollah Mosque",
+        "fa": "",
+        "img": "Sheikh Lotfollah Mosque.jpg"
+    },
+    {
+        "en": "Sultan Amir Ahmad Bath",
+        "fa": "",
+        "img": "Sultan Amir Ahmad Bath.jpg"
+    },
+    {
+        "en": "Tabataba\'ees\' House",
+        "fa": "",
+        "img": "Tabataba\'ees\' House.jpg"
+    },
+    {
+        "en": "Takht-e Soleyman",
+        "fa": "",
+        "img": "Takht-e Soleyman.jpg"
+    },
+    {
+        "en": "Takht-e Soleyman (2)",
+        "fa": "",
+        "img": "Takht-e Soleyman (2).jpg"
+    },
+    {
+        "en": "Taq-e Bostan (Bostan Arch)",
+        "fa": "",
+        "img": "Taq-e Bostan (Bostan Arch).jpg"
+    },
+    {
+        "en": "The Zagros Mountains",
+        "fa": "",
+        "img": "The Zagros Mountains.jpg"
+    },
+    {
+        "en": "Tomb of Ardeshir III",
+        "fa": "",
+        "img": "Tomb of Ardeshir III.jpg"
+    },
+    {
+        "en": "Tomb of Chalapi Oghli",
+        "fa": "",
+        "img": "Tomb of Chalapi Oghli.jpg"
+    },
+    {
+        "en": "Tomb of Hafez",
+        "fa": "",
+        "img": "Tomb of Hafez.jpg"
+    },
+    {
+        "en": "Tomb of Sa\'di (Sa\'dieh)",
+        "fa": "",
+        "img": "Tomb of Sa\'di (Sa\'dieh).jpg"
+    },
+    {
+        "en": "Tomb of Shah Ne\'matollah Vali",
+        "fa": "",
+        "img": "Tomb of Shah Ne\'matollah Vali.jpg"
+    },
+    {
+        "en": "Vakil Bathhouse",
+        "fa": "",
+        "img": "Vakil Bathhouse.jpg"
+    },
+    {
+        "en": "Vakil mosque",
+        "fa": "",
+        "img": "Vakil mosque.jpg"
+    },
+    {
+        "en": "Yakhdan-e Moayyedi (Moayyedi Ice Pitt)",
+        "fa": "",
+        "img": "Yakhdan-e Moayyedi (Moayyedi Ice Pitt).jpg"
+    },
+    {
+        "en": "Yazd Jame\' Mosque",
+        "fa": "",
+        "img": "Yazd Jame\' Mosque.jpg"
+    },
+    {
+        "en": "Yazd Jame\' Mosque (2)",
+        "fa": "",
+        "img": "Yazd Jame\' Mosque (2).jpg"
+    },
+    {
+        "en": "Zavareh bazzar",
+        "fa": "",
+        "img": "Zavareh bazzar.jpg"
+    },
+    {
+        "en": "Zavareh Hosseinieh",
+        "fa": "",
+        "img": "Zavareh Hosseinieh.jpg"
+    },
+    {
+        "en": "Zein-Abad Village",
+        "fa": "",
+        "img": "Zein-Abad Village.jpg"
+    },
+    {
+        "en": "Zia\'iyeh School",
+        "fa": "",
+        "img": "Zia\'iyeh School.jpg"
+    }
+]';
+$items = json_decode($data);
+//$items = array_reverse($items);
 ?>
 
 <div class="slideshow panorama">
 	<div class="img">
-		<img src="data/smpl4.jpg" alt="" />
+		<img src="<?php echo JURI::base(); ?>assets/data/panorama.jpg" alt="Panorama" />
 	</div>
 	<div class="caption-container">
 		<div class="row">
@@ -31,37 +466,20 @@ defined('_JEXEC') or die('Restricted access');
 		<div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
 			<div class="inner">
 				<ul class="items list-unstyled">
-					<li>
-						<div class="thumb"><img src="data/smpl4.jpg" alt="" /></div>
-						<div class="title">مسجد جامع اصفهان</div>
+					<?php foreach($items as $key => $item) { ?>
+					<li><?php $title = (!empty($item->fa)) ? $item->fa : $item->en; ?>
+						<div class="thumb"><img alt="<?php echo $title; ?>" src="<?php echo JURI::base() . 'assets/panorama/' . $item->img; ?>" /></div>
+						<div class="title"><?php echo $title; ?></div>
 					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl3.jpg" alt="" /></div>
-						<div class="title">حسینیه جماران</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl2.jpg" alt="" /></div>
-						<div class="title">مسجد شبخ لطف الله</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl1.jpg" alt="" /></div>
-						<div class="title">مسجد و مدرسه آقابزرگ</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl4.jpg" alt="" /></div>
-						<div class="title">باغ جهان نما</div>
-					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl3.jpg" alt="" /></div>
-					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl2.jpg" alt="" /></div>
-					</li>
-					<li>
-						<div class="thumb"><img src="data/smpl1.jpg" alt="" /></div>
-					</li>
+					<?php } ?>
 				</ul>
 			</div>
+		</div>
+	</div>
+	<div class="preview" style="display: none;">
+		<div class="inner" style="display: none;">
+			<div class="close">&times;</div>
+			<div class="player"></div>
 		</div>
 	</div>
 </div>
