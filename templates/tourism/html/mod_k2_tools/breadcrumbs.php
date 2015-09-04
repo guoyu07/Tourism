08fa03d5a344c98008e72d0dd84460a89d2bc552
Farid Rn <faridv@gmail.com>
2015-09-04 20:45:07 +0430

    Working on breadcrumbs module view

--- templates/tourism/html/mod_k2_tools/breadcrumbs.php
+++ templates/tourism/html/mod_k2_tools/breadcrumbs.php
@@ -6,38 +6,37 @@
  * @copyright	Copyright (c) 2006 - 2013 JoomlaWorks Ltd. All rights reserved.
  * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
-
 // no direct access
 defined('_JEXEC') or die;
-
 ?>
 
-<div id="k2ModuleBox<?php echo $module->id; ?>" class="k2BreadcrumbsBlock<?php if($params->get('moduleclass_sfx')) echo ' '.$params->get('moduleclass_sfx'); ?>">
-	<?php
-	$output = '';
-	if ($params->get('home')) {
-		$output .= '<span class="bcTitle">'.JText::_('K2_YOU_ARE_HERE').'</span>';
-		$output .= '<a href="'.JURI::root().'">'.$params->get('home',JText::_('K2_HOME')).'</a>';
-		if (count($path)) {
-			foreach ($path as $link) {
-				$output .= '<span class="bcSeparator">'.$params->get('seperator','&raquo;').'</span>'.$link;
-			}
+<ol class="breadcrumb">
+<?php
+$output = '';
+if ($params->get('home')) {
+	$output .= '<li><a href="' . JURI::root() . '">' . $params->get('home', JText::_('K2_HOME')) . '</a></li>';
+	if (count($path)) {
+		foreach ($path as $link) {
+//			$output .= '<span class="bcSeparator">' . $params->get('seperator', '&raquo;') . '</span>' . $link;
+			$output .= '<li>' . $link . '</li>';
 		}
-		if($title){
-			$output .= '<span class="bcSeparator">'.$params->get('seperator','&raquo;').'</span>'.$title;
-		}
-	} else {
-		if($title){
-			$output .= '<span class="bcTitle">'.JText::_('K2_YOU_ARE_HERE').'</span>';
-		}
-		if (count($path)) {
-			foreach ($path as $link) {
-				$output .= $link.'<span class="bcSeparator">'.$params->get('seperator','&raquo;').'</span>';
-			}
-		}
-		$output .= $title;
 	}
+//	if ($title) {
+//		$output .= '<span class="bcSeparator">' . $params->get('seperator', '&raquo;') . '</span>' . $title;
+//	}
+} else {
+//	if ($title) {
+//		$output .= '<span class="bcTitle">' . JText::_('K2_YOU_ARE_HERE') . '</span>';
+//	}
+	if (count($path)) {
+		foreach ($path as $link) {
+//			$output .= $link . '<span class="bcSeparator">' . $params->get('seperator', '&raquo;') . '</span>';
+			$output .= '<li>' . $link . '</li>';
+		}
+	}
+	$output .= $title;
+}
 
-	echo $output;
-	?>
-</div>
+echo $output;
+?>
+</ol>
