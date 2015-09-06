2e4a017b3a31dab9ddd39a741e630c441f450f94
Farid Rn <faridv@gmail.com>
2015-09-06 23:32:02 +0430

    Removed temp files

--- tmp/install_55c0d47c6d95e/k2-v3-dev-build-master/media/k2app/app/views/404.js
+++
@@ -1,8 +0,0 @@
-define(['marionette', 'text!templates/404.html'], function(Marionette, template) {'use strict';
-
-	var K2View404 = Marionette.ItemView.extend({
-		template : _.template(template)
-	});
-
-	return K2View404;
-});
