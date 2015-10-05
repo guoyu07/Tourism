61e67ab110106fa1223611f807fc16ed4390943a
Farid Rn <faridv@gmail.com>
2015-10-05 17:38:15 +0330

    Updated Joomla to 3.4.4

---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-10-05 13:48:52 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-10-05T13:48:52+00:00	INFO 127.0.0.1	update	Update started by user Super User (550). Old version is 3.4.3.
+2015-10-05T13:48:53+00:00	INFO 127.0.0.1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.4/Joomla_3.4.3_to_3.4.4-Stable-Patch_Package.zip.
+2015-10-05T13:49:03+00:00	INFO 127.0.0.1	update	File Joomla_3.4.3_to_3.4.4-Stable-Patch_Package.zip successfully downloaded.
+2015-10-05T13:49:03+00:00	INFO 127.0.0.1	update	Starting installation of new version.
+2015-10-05T13:49:15+00:00	INFO 127.0.0.1	update	Finalising installation.
+2015-10-05T13:49:15+00:00	INFO 127.0.0.1	update	Deleting removed files and folders.
+2015-10-05T13:49:16+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
+2015-10-05T13:49:16+00:00	INFO 127.0.0.1	update	Update to version 3.4.4 is complete.
