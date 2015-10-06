136da68c793445105e8fe9305234ff4211c3b88f
Farid Rn <faridv@gmail.com>
2015-10-06 16:26:57 +0330

    Added AllVideos plugin

---
+++ plugins/content/jw_allvideos/jw_allvideos/tmpl/Tourism/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version		4.6.1
+ * @package		AllVideos (plugin)
+ * @author    	JoomlaWorks - http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+?>
+<?php echo $tagsource; ?>
