3694bdb5afed3f7c775eb8d440208527acaced43
Farid Rn <faridv@gmail.com>
2015-10-24 20:32:56 +0330

    Updated Joomla to 3.4.5

--- logs/joomla_update.php
+++ logs/joomla_update.php
@@ -12,3 +12,11 @@
 2015-10-05T13:49:15+00:00	INFO 127.0.0.1	update	Deleting removed files and folders.
 2015-10-05T13:49:16+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
 2015-10-05T13:49:16+00:00	INFO 127.0.0.1	update	Update to version 3.4.4 is complete.
+2015-10-24T17:00:37+00:00	INFO ::1	update	Update started by user Super User (550). Old version is 3.4.4.
+2015-10-24T17:00:38+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.5/Joomla_3.4.4_to_3.4.5-Stable-Patch_Package.zip.
+2015-10-24T17:00:45+00:00	INFO ::1	update	File Joomla_3.4.4_to_3.4.5-Stable-Patch_Package.zip successfully downloaded.
+2015-10-24T17:00:45+00:00	INFO ::1	update	Starting installation of new version.
+2015-10-24T17:00:47+00:00	INFO ::1	update	Finalising installation.
+2015-10-24T17:00:47+00:00	INFO ::1	update	Deleting removed files and folders.
+2015-10-24T17:00:48+00:00	INFO ::1	update	Cleaning up after installation.
+2015-10-24T17:00:48+00:00	INFO ::1	update	Update to version 3.4.5 is complete.
