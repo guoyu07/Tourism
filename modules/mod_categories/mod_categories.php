be729ab050f3e711bc7949522632a1b3772d184c
Farid Rn <faridv@gmail.com>
2016-01-03 12:13:24 +0330

    Added template chooser to categories module

--- modules/mod_categories/mod_categories.php
+++ modules/mod_categories/mod_categories.php
@@ -11,7 +11,8 @@ defined('_JEXEC') or die('Restricted access'); // no direct access
 
 require_once __DIR__ . '/helper.php';
 $item = modCategoriesHelper::getItem($params);
-require(JModuleHelper::getLayoutPath('mod_categories'));
+//require(JModuleHelper::getLayoutPath('mod_categories'));
+require JModuleHelper::getLayoutPath('mod_categories', $params->get('layout', 'default'));
 require_once ('helper.php');
 
 ?>
